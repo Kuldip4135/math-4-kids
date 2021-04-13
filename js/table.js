df7851f5b570c8fc9d00f5bf0ca01b406db8ce70
Kuldip4135 <thakkarkuldip29@gmail.com>
2021-04-14 01:16:20 +0530

    Nea Feature Added

---
+++ js/table.js
@@ -0,0 +1,89 @@
+const userInput = document.getElementById("userInput");
+const form = document.querySelector("form");
+const table = document.querySelector("table");
+const header = document.querySelector("header");
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+
+hamburgerMenu.addEventListener("click", () => {
+  header.classList.toggle("active");
+});
+
+form.addEventListener("submit", generateTable);
+
+function generateTable(e) {
+  e.preventDefault();
+
+  if (isNaN(userInput.value)) {
+    return alert("Only Numbers Are Allowed");
+  }
+
+  for (let i = 1; i <= 10; i++) {
+    table.innerHTML = `
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${1}</td>
+    <td>${userInput.value * 1}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${2}</td>
+    <td>${userInput.value * 2}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${3}</td>
+    <td>${userInput.value * 3}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${4}</td>
+    <td>${userInput.value * 4}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${5}</td>
+    <td>${userInput.value * 5}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${6}</td>
+    <td>${userInput.value * 6}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${7}</td>
+    <td>${userInput.value * 7}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${8}</td>
+    <td>${userInput.value * 8}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${8}</td>
+    <td>${userInput.value * 8}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${9}</td>
+    <td>${userInput.value * 9}</td>
+    </tr>
+
+    <tr>
+    <td>${userInput.value}</td>
+    <td>${10}</td>
+    <td>${userInput.value * 10}</td>
+    </tr>
+    `;
+  }
+}
